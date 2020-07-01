2152b913a333d3bd0640532d3410fa44d36d0383
Britton Riggs <bdriggs01@gmail.com>
2020-06-30 20:12:57 -0400

    Full API with authentication.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 //import { AppController } from './app.controller';
 //import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+import { AuthzModule } from './authz/authz.module';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule, AuthzModule],
   controllers: [],
   providers: [],
 })
